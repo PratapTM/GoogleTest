f579a7e231c564695427511eb316e6cde13e5153
Pratap <pratap.tm@epsilon.com>
2024-11-22 21:51:57 +0530

    Running Chrome Headless

--- src/main/java/com/google/baseTest/BaseTestClass.java
+++ src/main/java/com/google/baseTest/BaseTestClass.java
@@ -7,26 +7,30 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTestClass {
-	
+
 	public static WebDriver driver;
 
 	public WebDriver initilaztionDriver() throws IOException {
 
 		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\Global.properties");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\Global.properties");
 		prop.load(fis);
 		String browser = prop.getProperty("browser");
 		String url = prop.getProperty("url");
 
 		if (browser.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(options);
+//			driver.set(new ChromeDriver(options));
 
 		} else if (browser.equalsIgnoreCase("edge")) {
 
@@ -34,15 +38,15 @@ public class BaseTestClass {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get(url);
-		return driver;		
+		return driver;
 
 	}
-	
+
 	@BeforeSuite
 	public void lunchApp() throws IOException {
-		driver=initilaztionDriver();
+		driver = initilaztionDriver();
 	}
-	
+
 	@AfterSuite
 	public void closeApp() {
 		driver.quit();
